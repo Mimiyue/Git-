ba6915e0949562a2814efc0e125cdd4c6b61d67c
Mimiyue <weiwenxue1984@163.com>
2021-01-10 20:35:48 +0800

    pytest

--- wenapp.py
+++ wenapp.py
@@ -1,21 +1,27 @@
 from selenium import webdriver
 import unittest
+import pytest
+# class jenkinsTest(unittest.TestCase):
+print("start testing")
+def test_01():
+    print("hello Git")
+def test_02():
+    print("hello Git1")
+    print("hello Git2")
+    print("hello Git3")
+def test_03():
+    print("hello Git4")
 
-class jenkinsTest(unittest.TestCase):
-    def test01(self):
-        print("hello Git")
-    def test02(self):
-        print("hello Git1")
-        print("hello Git2")
-        print("hello Git3")
-    def test03(self):
-        print("hello Git4")
+import pytest
+pytest.main(['wenapp.py','-vv','--alluredir=./allure-results'])
 
-if __name__ == '__main__':
-    suite = unittest.TestSuite()
-    loader = unittest.TestLoader()
-    cases = loader.loadTestsFromTestCase(jenkinsTest)
-    suite.addTests(cases)
-    runner = unittest.TextTestRunner(stream=open('res.txt', 'a'), verbosity=2)
-    runner.run(suite)
+
+# if __name__ == '__main__':
+#     unittest.main(['wenapp.py','-vv','--alluredir=./allure-results'])
+    # suite = unittest.TestSuite()
+    # loader = unittest.TestLoader()
+    # cases = loader.loadTestsFromTestCase(jenkinsTest)
+    # suite.addTests(cases)
+    # runner = unittest.TextTestRunner(stream=open('res.txt', 'a'), verbosity=2)
+    # runner.run(suite)
 
