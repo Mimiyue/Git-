afab258681da537825c712deb7b5fde455c9299e
Mimiyue <weiwenxue1984@163.com>
2021-01-10 17:07:41 +0800

    追加jenkinsTest

--- wenapp.py
+++ wenapp.py
@@ -1,7 +1,22 @@
 from selenium import webdriver
+import unittest
 
-print("hello Git")
-print("hello Git1")
-print("hello Git2")
-print("hello Git3")
-print("hello Git4")
+class jenkinsTest(unittest.TestCase):
+    def test01(self):
+        print("hello Git")
+    def test02(self):
+        print("hello Git1")
+        print("hello Git2")
+        print("hello Git3")
+    def test03(self):
+        print("hello Git4")
+
+if __name__ == '__main__':
+    # suite = unittest.TestSuite()
+    # loader = unittest.TestLoader()
+    # cases = loader.loadTestsFromTestCase(jenkinsTest)
+    # suite.addTests(cases)
+    # runner = unittest.TextTestRunner(stream=open('res.txt', 'a'), verbosity=2)
+    # runner.run(suite)
+    unittest.main()
+
